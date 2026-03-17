506ca105e5aa9e7cc234e0c6d1f257bb93051ddf
Abhinav5383 <itsabhinav5383@gmail.com>
2026-03-17 11:09:22 +0530

    remove redundant log

--- src/helpers/bot-utils.ts
+++ src/helpers/bot-utils.ts
@@ -43,7 +43,6 @@ export class BotUtilities {
 
     async isSectionCompleted(section: Locator) {
         const headerText = await this.getSectionHeaderText(section);
-        console.log(headerText);
 
         if (!headerText?.trim() || headerText.toLowerCase().startsWith("complete")) {
             return true;
